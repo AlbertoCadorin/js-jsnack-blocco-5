b1330be73189233d32de190cf1c55fdb6cd3da44
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-12 14:35:03 +0100

    feat: snack 5 completed

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,6 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
+const numEven = nums.filter((number) =>{return number % 2 === 0});
+console.log(numEven);
 // Risultato: [2, 8, 4, 12]
