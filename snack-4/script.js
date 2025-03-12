10e813e3fdc15911e5f9c31f350049b4768fe4f2
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-12 14:31:01 +0100

    feat: snack 4 commpleted

--- snack-4/script.js
+++ snack-4/script.js
@@ -6,4 +6,6 @@ const posts = [
 ]
 
 // Crea un nuovo array coi nomi di tutti gli autori
+const nameAuthor = posts.map((name) => {return name.author});
+console.log(nameAuthor);
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
